a9a9c5dd68918e029e8d6afb1bf998e51009d6f1
lrj <1849590021@qq.com>
2021-12-25 22:41:32 +0800

    add camera

--- sketch.js
+++ sketch.js
@@ -1,42 +1,74 @@
 let particles = [];
+let camSpeed = 1;
+let cam;
+let fov = 45;
+
+function mouseWheel(event) {
+    fov -= event.delta * 0.01;
+    fov = max(10, fov);
+    fov = min(45, fov);
+}
 
 function setup() {
-  createCanvas(1280, 720);
-  startStop();
-  colorMode(HSB);
-  stroke(255);
-  strokeWeight(4);
-  background(0);
+    createCanvas(1280, 720, WEBGL);
+    startStop();
+    colorMode(HSB);
+    angleMode(DEGREES);
+    stroke(255);
+    strokeWeight(4);
+    background(0);
+    cam = createCamera();
 }
 
 function draw() {
-  colorMode(RGB);
-  background(0, 0, 0, 25);
+    colorMode(RGB);
+    background(0, 0, 0);
 
-  if (random(1000) < 15) {
-    particles.push(new Firework());
-  }
+    perspective(fov);
+    cameraMove();
+    box();
 
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particles[i].show();
-    if (particles[i].done()) {
-      particles.push(...particles[i].explosion());
-      particles.splice(i, 1);
+    if (random(1000) < 15) {
+        particles.push(new Firework());
     }
-  }
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].show();
+        if (particles[i].done()) {
+            particles.push(...particles[i].explosion());
+            particles.splice(i, 1);
+        }
+    }
 }
 
 function startStop() {
-  let div = createDiv();
-  let button = createButton("start");
-  let button2 = createButton("stop");
+    let div = createDiv();
+    let button = createButton("start");
+    let button2 = createButton("stop");
 
-  button.parent(div);
-  button2.parent(div);
-  div.center("horizontal");
-  // noLoop();
+    button.parent(div);
+    button2.parent(div);
+    div.center("horizontal");
+    // noLoop();
 
-  button.mousePressed(loop);
-  button2.mousePressed(noLoop);
+    button.mousePressed(loop);
+    button2.mousePressed(noLoop);
+}
+
+
+function cameraMove() {
+    orbitControl();  // Drag mouse to change the view angle
+    if (keyIsDown(65)) {  // Use key'A'to make a left move
+        cam.move(-camSpeed, 0, 0);
+    }
+    if (keyIsDown(68)) {  // Use key'D'to make a left move
+        cam.move(camSpeed, 0, 0);
+    }
+    if (keyIsDown(87)) {  // Use key'W'to make a forward move
+        cam.move(0, 0, -camSpeed);
+    }
+    if (keyIsDown(83)) {  // Use key'S'to make a backward move
+        cam.move(0, 0, camSpeed);
+    }
 }
